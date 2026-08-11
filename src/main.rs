82dbf7a993e1e3b31883c74f17852ca531547c6e
EdvinAlvarado <ealvarado301@gmail.com>
2026-08-11 00:03:01 -0500

    Switched custom written ping with crate ping.

--- src/main.rs
+++ src/main.rs
@@ -4,40 +4,25 @@ use main_error::MainError;
 use std::path::PathBuf;
 use std::sync::Arc;
 use thiserror::Error;
-use tokio::process::Command;
 use tokio::sync::Semaphore;
+use std::net::IpAddr;
+use std::io::BufRead;
+use std::net::ToSocketAddrs;
 
 #[derive(Error, Debug)]
 enum ScanError {
     #[error("Wrong Arguments. Check whether input method was mixed.")]
     WrongArguments,
     #[error("Ping command incorrect or no ping exe available.")]
-    PingCommandError(#[from] std::io::Error),
+    IoError(#[from] std::io::Error),
     #[error("Output is not utf8.")]
     PingOutputUtf8Error(#[from] std::string::FromUtf8Error),
-    #[error("Output did not return True or False")]
-    PingOutputError((String, String)),
+    #[error("Could not parse IP address.")]
+    AddressError(#[from] std::net::AddrParseError),
+    #[error("Could not ping")]
+    PingError(#[from] ping::Error),
 }
 
-async fn ping(ip: String) -> Result<Option<String>, ScanError> {
-    let test_cmd = format!(
-        "Test-NetConnection {ip} | Select -ExpandProperty \"PingSucceeded\"| echo"
-    );
-    let mut cmd = Command::new("powershell");
-    cmd.arg("-Command");
-    cmd.arg(test_cmd);
-
-    let raw_output = cmd.output().await?.stdout;
-    let output = String::from_utf8(raw_output)?;
-
-    if output.contains("True") {
-        Ok(Some(ip))
-    } else if output.contains("False") {
-        Ok(None)
-    } else {
-        Err(ScanError::PingOutputError((ip, output)))
-    }
-}
 
 /// Scan ips or hostnames to see if pingable.
 /// Scan an IP range from to.
@@ -57,18 +42,30 @@ struct Args {
     pipe: bool,
 }
 
+fn string_to_ipaddr(s: String) -> Result<IpAddr, ScanError> {
+    let ip= s.parse();
+    match ip {
+        Ok(ip) => Ok(ip),
+        Err(e) => Ok(s.to_socket_addrs()?.next().ok_or(ScanError::AddressError(e))?.ip()),
+    }
+}
+
 #[tokio::main]
 async fn main() -> Result<(), MainError> {
     let args = Args::parse();
-    let hosts: Vec<String> = match (args.from, args.to, args.file, args.pipe) {
+    let hosts: Vec<IpAddr> = match (args.from, args.to, args.file, args.pipe) {
         (Some(from), Some(to), None, false) => Ipv4AddrRange::new(from.parse()?, to.parse()?)
-            .map(|ip| ip.to_string())
+            .map(Into::into)
             .collect(),
         (None, None, Some(file_path), false) => std::fs::read_to_string(file_path)?
             .lines()
             .map(std::string::ToString::to_string)
-            .collect(),
-        (None, None, None, true) => std::io::stdin().lines().collect::<Result<Vec<String>, _>>()?,
+            .map(string_to_ipaddr)
+            .collect::<Result<Vec<IpAddr>, _>>()?,
+        (None, None, None, true) => std::io::stdin().lock()
+                                                    .lines()
+                                                    .map(|rstring| rstring.map_err(ScanError::IoError).and_then(string_to_ipaddr))
+            .collect::<Result<Vec<IpAddr>, ScanError>>()?,
         _ => {
             return Err(ScanError::WrongArguments.into());
         }
@@ -80,16 +77,15 @@ async fn main() -> Result<(), MainError> {
         let permit = semaphore.clone().acquire_owned().await?;
         let t = tokio::spawn(async move {
             let _permit = permit; // Keep the permit alive for the duration of the task
-            ping(host).await
+            ping::new(host).send()
         });
         tasks.push(t);
     }
 
     for task in tasks {
         match task.await? {
-            Ok(Some(host)) => println!("{host}"),
-            Ok(None)  => (),
-            Err(ScanError::PingOutputError((host, output))) => eprintln!("Error pinging {host}: {output}"),
+            Ok(pr) => println!("{}", pr.source),
+            Err(ping::Error::IoError { error: _ }) => (),
             Err(e) => eprintln!("Error pinging: {e}"),
         }
     }
